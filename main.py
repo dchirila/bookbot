67fbd1510ab43bda50768f75be4ac804771141c2
Dragos B. Chirila <dchirila@gmail.com>
2025-08-26 23:46:50 +0300

    implement book text retrieval and word counting functionality

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    file_contents = ""
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
 
+def count_words(book_text):
+    return len(book_text.split())
+
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    print(f"{count_words(book_text)} words found in the document")
+
+main()
