97f3a3af3377ca9b0417df1083cc5248612394d9
Dragos B. Chirila <dchirila@gmail.com>
2025-08-28 23:52:05 +0300

    Allow passing book path as CLI argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, per_symbol_stats, get_sorted_list_of_dicts, expand_stats
 
 def get_book_text(path_to_file):
@@ -7,7 +8,12 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:  # correct number of arguments
+        book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
 
